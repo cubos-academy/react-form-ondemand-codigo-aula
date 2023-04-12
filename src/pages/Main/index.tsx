166ec3a8e5f2aecb0953191aae16964afd22a893
Daniel <danieldeandradelopes@gmail.com>
2023-04-12 00:26:04 -0300

    finish classe code

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import "./styles.css";
 
 const allCoursesOptions = [
@@ -21,17 +21,34 @@ const allCoursesOptions = [
   },
 ];
 
+const defaultForm = {
+  name: "",
+  email: "",
+  age: 0,
+  password: "",
+};
+
 function App() {
-  const [inputName, setInputName] = useState("");
-  const [selectCourses, setSelectCourses] = useState({ id: 0, name: "" });
+  const [form, setForm] = useState({ ...defaultForm });
+  const [selectCourses, setSelectCourses] = useState<{
+    id: number;
+    name: string;
+  } | null>(null);
+
   const [radioPeriod, setRadioPeriod] = useState("morning");
+  const [acceptTerms, setAcceptTerms] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   function handleSelectCourse(event: ChangeEvent<HTMLSelectElement>) {
     const currentSelectedCourse = allCoursesOptions.find(
       (course) => course.id === parseInt(event.target.value)
     );
 
-    if (!currentSelectedCourse) return;
+    if (!currentSelectedCourse) {
+      setSelectCourses(null);
+      return;
+    }
 
     setSelectCourses(currentSelectedCourse);
   }
@@ -40,58 +57,152 @@ function App() {
     setRadioPeriod(event.target.value);
   }
 
+  function handleFillForm(event: ChangeEvent<HTMLInputElement>) {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  }
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    setErrorMessage("");
+
+    const entriesForm = Object.entries(form);
+
+    entriesForm.forEach((item) => {
+      if (!item[1]) {
+        setErrorMessage(`Preencha o campo ${item[0]}`);
+        return;
+      }
+    });
+
+    if (!acceptTerms) {
+      setErrorMessage("Você precisa aceitar os termos!");
+      return;
+    }
+
+    if (!selectCourses) {
+      setErrorMessage("Você não selecionou um curso");
+      return;
+    }
+
+    const data = {
+      ...form,
+      course: selectCourses,
+      period: radioPeriod,
+      terms: acceptTerms,
+    };
+
+    console.log(data);
+  }
+
+  function handleReset() {
+    setForm({ ...defaultForm });
+    setSelectCourses(null);
+    setRadioPeriod("morning");
+    setAcceptTerms(false);
+  }
+
   return (
     <div className="App">
-      <h1>{inputName}</h1>
-      <input
-        type="text"
-        placeholder="Digite seu nome"
-        value={inputName}
-        onChange={(event) => setInputName(event.target.value)}
-      />
+      <form onSubmit={handleSubmit}>
+        <input
+          name="name"
+          type="text"
+          placeholder="Digite seu nome"
+          value={form.name}
+          onChange={handleFillForm}
+        />
 
-      <select id="courses" onChange={handleSelectCourse}>
-        <option value="">Selecione um curso</option>
-        {allCoursesOptions.map((course) => (
-          <option key={course.id} value={course.id}>
-            {course.name}
-          </option>
-        ))}
-      </select>
+        <input
+          name="email"
+          type="text"
+          placeholder="Digite seu e-mail"
+          value={form.email}
+          onChange={handleFillForm}
+        />
 
-      <h2>{radioPeriod}</h2>
+        <input
+          name="age"
+          type="number"
+          placeholder="Digite sua idade"
+          value={form.age}
+          onChange={handleFillForm}
+        />
 
-      <div className="radio-group">
-        <label>
+        <select id="courses" onChange={handleSelectCourse}>
+          <option value="">Selecione um curso</option>
+          {allCoursesOptions.map((course) => (
+            <option key={course.id} value={course.id}>
+              {course.name}
+            </option>
+          ))}
+        </select>
+
+        <input
+          name="password"
+          type={showPassword ? "text" : "password"}
+          placeholder="Digite sua senha"
+          value={form.password}
+          onChange={handleFillForm}
+        />
+
+        <div className="terms">
+          <span>{!showPassword ? "Mostrar" : "Ocultar"} senha</span>
           <input
-            type="radio"
-            value="morning"
-            checked={radioPeriod === "morning"}
-            onChange={handleSelectRadio}
+            type="checkbox"
+            checked={showPassword}
+            onChange={() => setShowPassword(!showPassword)}
           />
-          Manhã
-        </label>
+        </div>
 
-        <label>
-          <input
-            type="radio"
-            value="afternoon"
-            checked={radioPeriod === "afternoon"}
-            onChange={handleSelectRadio}
-          />
-          Tarde
-        </label>
+        <div className="radio-group">
+          <label>
+            <input
+              type="radio"
+              value="morning"
+              checked={radioPeriod === "morning"}
+              onChange={handleSelectRadio}
+            />
+            Manhã
+          </label>
 
-        <label>
+          <label>
+            <input
+              type="radio"
+              value="afternoon"
+              checked={radioPeriod === "afternoon"}
+              onChange={handleSelectRadio}
+            />
+            Tarde
+          </label>
+
+          <label>
+            <input
+              type="radio"
+              value="night"
+              checked={radioPeriod === "night"}
+              onChange={handleSelectRadio}
+            />
+            Noite
+          </label>
+        </div>
+
+        <div className="terms">
+          <span>Aceita os termos de uso? </span>
           <input
-            type="radio"
-            value="night"
-            checked={radioPeriod === "night"}
-            onChange={handleSelectRadio}
+            type="checkbox"
+            checked={acceptTerms}
+            onChange={() => setAcceptTerms(!acceptTerms)}
           />
-          Noite
-        </label>
-      </div>
+        </div>
+
+        <button>Salvar</button>
+        <button type="button" onClick={handleReset}>
+          Limpar
+        </button>
+
+        {errorMessage && <strong>{errorMessage}</strong>}
+      </form>
     </div>
   );
 }
